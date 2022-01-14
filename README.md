15d7066cf272be42015544e383b1e28f2ef9474a
sleepbook53 <sleepbook53@gmail.com>
2022-01-14 17:09:20 +0900

    Python 1day 진행

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Studypython
 빅데이터반 파이썬 학습 리포지토리
+
+
+### 1일차
+- 프로그래밍 이해
+- 파이썬 설치, Visual Studio Coda 설치
+- 개발환경 학습
+- 기본 문법
+  - 콘솔출력 HEllo world
+  - 사칙연산
+  - type()함수
+  - int(), flat()형변환 함수
+  
---
+++ day1/Basic syntxt01.py
@@ -0,0 +1,37 @@
+# 이것은 주석이라고 한다 
+#문자 열 표현 방법 syntxt
+print('Hellow World!') # 가능
+print("Hellow World!") # 가능
+#print('Hellow World!") #요렇게는 안된다
+
+#숫자출력
+#pint( )
+print(3 + 4)
+
+#변수사용
+a = '안녕하세요'
+b = '반갑습니다'
+print(a)
+print(b)
+print(a,b)
+
+#수학 연산
+print(5 + 6)
+print(5 - 6)
+print(5 * 6)
+print(5 / 3)
+print(5 // 3) #정수로 나누는 것
+print(5 % 3) #남은 값
+print(2 ** 10) #제곱
+print(int(5 / 3)) #int=정수로 표현
+print(int(10))
+#print(int('십')) 이런 문자 안됨
+print(float(5)) #실수
+
+#type 확인
+print(type(10))
+print(type(10.0))
+
+# 괄호(연산 우선순위)
+print(2 + (3 * 5))
+print((2 + 3) * 5)
